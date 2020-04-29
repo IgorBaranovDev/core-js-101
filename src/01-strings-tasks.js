f0f9fb6b1b5effaedcbc16b52c210746e64fb7df
Baranov Igor <igor.82bv@mail.ru>
2020-04-29 23:20:40 +0300

    update: string & numbers finished

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -242,8 +242,8 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 /**
@@ -270,8 +270,123 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  // const arrCards = [
+  //   'A♣',
+  //   '2♣',
+  //   '3♣',
+  //   '4♣',
+  //   '5♣',
+  //   '6♣',
+  //   '7♣',
+  //   '8♣',
+  //   '9♣',
+  //   '10♣',
+  //   'J♣',
+  //   'Q♣',
+  //   'K♣',
+  //   'A♦',
+  //   '2♦',
+  //   '3♦',
+  //   '4♦',
+  //   '5♦',
+  //   '6♦',
+  //   '7♦',
+  //   '8♦',
+  //   '9♦',
+  //   '10♦',
+  //   'J♦',
+  //   'Q♦',
+  //   'K♦',
+  //   'A♥',
+  //   '2♥',
+  //   '3♥',
+  //   '4♥',
+  //   '5♥',
+  //   '6♥',
+  //   '7♥',
+  //   '8♥',
+  //   '9♥',
+  //   '10♥',
+  //   'J♥',
+  //   'Q♥',
+  //   'K♥',
+  //   'A♠',
+  //   '2♠',
+  //   '3♠',
+  //   '4♠',
+  //   '5♠',
+  //   '6♠',
+  //   '7♠',
+  //   '8♠',
+  //   '9♠',
+  //   '10♠',
+  //   'J♠',
+  //   'Q♠',
+  //   'K♠',
+  // ];
+  // let result;
+
+  // for (let i = 0; i < arrCards.length; i += 1) {
+  //   if (value === arrCards[i]) {
+  //     result = i;
+  //   }
+  // }
+  // return result;
+  return [
+    'A♣',
+    '2♣',
+    '3♣',
+    '4♣',
+    '5♣',
+    '6♣',
+    '7♣',
+    '8♣',
+    '9♣',
+    '10♣',
+    'J♣',
+    'Q♣',
+    'K♣',
+    'A♦',
+    '2♦',
+    '3♦',
+    '4♦',
+    '5♦',
+    '6♦',
+    '7♦',
+    '8♦',
+    '9♦',
+    '10♦',
+    'J♦',
+    'Q♦',
+    'K♦',
+    'A♥',
+    '2♥',
+    '3♥',
+    '4♥',
+    '5♥',
+    '6♥',
+    '7♥',
+    '8♥',
+    '9♥',
+    '10♥',
+    'J♥',
+    'Q♥',
+    'K♥',
+    'A♠',
+    '2♠',
+    '3♠',
+    '4♠',
+    '5♠',
+    '6♠',
+    '7♠',
+    '8♠',
+    '9♠',
+    '10♠',
+    'J♠',
+    'Q♠',
+    'K♠',
+  ].indexOf(value);
 }
 
 module.exports = {
